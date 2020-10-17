d497f50bd305b99fc2cc068e448f3a423c76f45b
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:43:17 +0700

    PBOIF2-10119076-Latihan16-Penugasan

---
+++ src/pboif2/pkg10119076/latihan16/penugasan/PBOIF210119076Latihan16Penugasan.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan16.penugasan;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : Penugasan
+ */
+public class PBOIF210119076Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c/=a;
+        System.out.println("c /= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c %=a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+        
+        
+        
+        
+        
+    }
+    
+}
